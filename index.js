2df99868a817e2757005c7bd2db40befd579ce24
Jibin Mathews <jibinmathews7@gmail.com>
2016-09-26 17:59:31 +0530

    Improvied code

--- index.js
+++ index.js
@@ -17,22 +17,14 @@ process.on('unhandledException', (error, m)=> {
   console.log("Unhandled Exception at: Error ", m, " reason: ", error);
 });
 
-
 var transporter = nodemailer.createTransport('smtps://'+user+':'+password+'@smtp.gmail.com');
 
 inquirer.prompt([{
-  name: 'subject',
-  message: "Subject: ",
-  validate: function(ip){
-    var done = this.async();
-    if(ip.length<5){
-      return done("Subject cannot be less than 5 characters long", false);
-    }
-    return done(null, true);
-  }
+  name: 'name',
+  message: "Reciever name: ",
 },{
   name: 'email',
-  message: "Receiver Email: ",
+  message: "Receiver Email *: ",
   validate: function(ips){
     var done = this.async();
     var emails = ips.split(',');
@@ -48,11 +40,18 @@ inquirer.prompt([{
     return done(null, true);
   }
 },{
-  name: 'name',
-  message: "Reciever name: ",
+  name: 'subject',
+  message: "Subject *: ",
+  validate: function(ip){
+    var done = this.async();
+    if(ip.length<5){
+      return done("Subject cannot be less than 5 characters long", false);
+    }
+    return done(null, true);
+  }
 },{
   name: 'company',
-  message: "Company: ",
+  message: "Company *: ",
   validate: function(ip){
     var done = this.async();
     if(ip.length<=2){
@@ -62,29 +61,37 @@ inquirer.prompt([{
   }
 },{
   name: "position",
-  message: "What role are you applying for? "
+  message: "What role are you applying for? ",
+  default: "Software Developer"
 },{
   type: "list",
   name: "template",
-  choices: ['LinkedIn Listing - Cover Letter', 'Standard Cover Letter'],
-  default: 1,
+  choices: ['LinkedIn Listing', 'LinkedIn Listing With CTC', 'Standard Cover Letter', 'Other'],
+  default: 0,
   message: "Which template to use? "
-},{
-  name: "temporaryTemplate",
-  default: '',
-  message: "Any other template apart from current specified?"
 }]).then(function(answers){
   console.log(chalk.red("----------------Answers------------------"));
   console.log(answers);
-  console.log(chalk.red('------------------------------------------'));
+  console.log(chalk.red("-----------------------------------------"));
+  processAnswers(answers);
+});
+
+
+
+function processAnswers(answers){
+  //Name Validation
   if(!answers.name){
     answers.name = "";
   }else{
     answers.name = " "+answers.name;
   }
+
+  //Role setup
   if(!answers.position || answers.position.length<=3){
     answers.position = "Software Developer";
   }
+
+  //Email splitting
   if(answers.email.indexOf(',')!==-1){
     answers.email = answers.email.split(',');
     for(var i=0;i<answers.email.length;i++){
@@ -92,39 +99,53 @@ inquirer.prompt([{
     }
     answers.name = "";
   }
+
+  //Company Name setup
   if(answers.company){
     answers.company = " at "+answers.company;
   }
-  var file = "letter";
-  if(answers.template==="LinkedIn Listing - Cover Letter"){
-    file = "cover-linkedIn";
+
+  switch(answers.template){
+    case "LinkedIn Listing With CTC":
+      return getCTC(answers);
+      break;
+    case "Other":
+      return otherTemplate(answers);
+      break;
+    case "Standard Cover Letter":
+      answers.file = "letter";
+      break;
+    case "LinkedIn Listing":
+    default:
+      answers.file = "cover-linkedIn";
+      break;
   }
-  if(answers.temporaryTemplate){
-    file = answers.temporaryTemplate;
-  }
-  // var file = "old";
-  var textContent = fs.readFileSync(path.join(__dirname, 'templates', file+".txt")).toString();
-  textContent = textContent.replace(/<%= email.name %>/g, answers.name).replace(/<%= email.position %>/g,answers.position).replace(/<%= email.company %>/g, answers.company);
-  // var textContent = "";
-  ejs.renderFile(path.join(__dirname, 'templates', file+"-converted.html"),{email: answers},{}, function(err, str){
+  sendEmail(answers);
+}
+
+function sendEmail(answers){
+  var textContent = fs.readFileSync(path.join(__dirname, 'templates', answers.file+".txt")).toString();
+  textContent = textContent.replace(/<%= email.name %>/g, answers.name).replace(/<%= email.position %>/g, answers.position).replace(/<%= email.company %>/g, answers.company);
+
+  ejs.renderFile(path.join(__dirname, 'templates', answers.file+"-converted.html"),{email: answers},{}, function(err, str){
     var mailOptions = {
-      from: '"Jibin Mathews" <jibinmathews7@gmail.com>', // sender address
-      to: answers.email, // list of receivers
+      from: '"Jibin Mathews" <jibinmathews7@gmail.com>',
+      to: answers.email,
       bcc: 'jibinmathews7@gmail.com',
-      subject: answers.subject, // Subject line
+      subject: answers.subject,
       text: textContent,
-      html: str // html body
+      html: str
     };
-    // fs.writeFileSync('./sent.html', str);
+
     inquirer.prompt([{
       type: 'confirm',
       name: "send",
       default: false,
-      message: "Confirm sending email to: "+answers.name+" <"+answers.email+"> of "+answers.company
+      message: "Confirm sending email to: "+chalk.yellow(answers.name+" <"+answers.email+"> of "+answers.company)
     }]).then(function(sendConfirmation){
       console.log(chalk.red("-----------Send Confirmation--------------"));
       console.log(sendConfirmation);
-      console.log(chalk.red('------------------------------------------'));
+      console.log(chalk.red("------------------------------------------"));
       if(sendConfirmation.send){
         transporter.sendMail(mailOptions, function(error, info){
           if(error){
@@ -139,4 +160,45 @@ inquirer.prompt([{
       }
     });
   });
-});
+}
+
+function getCTC(answers){
+  inquirer.prompt([{
+    name: "lower",
+    message: "CTC Lower Limit: ",
+    default: '14.00'
+  },{
+    name: "upper",
+    message: "CTC Upper limit: ",
+    default: '20.00'
+  }]).then(function(ans){
+    for(var key of Object.keys(ans)){
+      answers[key] = ans[key];
+    }
+    answers.file = "linkedIn-withCTC";
+    sendEmail(answers);
+  });
+}
+
+function otherTemplate(answers){
+  inquirer.prompt([{
+    name: "file",
+    message: "Template file name (without extention)",
+    validate: function(ip){
+      var done = this.async();
+      var htmlFile = path.join(__dirname, 'templates', ip+"-converted.html");
+      var textFile = path.join(__dirname, 'templates', ip+".txt");
+      if(!fs.existsSync(htmlFile)){
+        return done("Converted html file does not exists. Read readme.md for more details on how to create templates", false);
+      }
+      if(!fs.existsSync(textFile)){
+        return done("Text file does not exists for this template. Read readme.md for more details", false);
+      }
+      return done(null, true);
+    }
+  }])
+  .then(function(ans){
+    answers.file = ans.file;
+    sendEmail(answers);
+  });
+}
